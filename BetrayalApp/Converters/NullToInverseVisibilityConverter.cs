471ee9e0fd3292fbac181d088e27d865d8a1c1e3
Daymian Tomczyk <daymian.tomczyk@outlook.com>
2019-10-12 19:53:09 -0400

    Progress Commit

---
+++ BetrayalApp/Converters/NullToInverseVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BetrayalApp.Converters
+{
+    class NullToInverseVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a null value to Visibility.Collapsed, else Visible.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
